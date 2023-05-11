519aea105cd69d0a4470de2ff9a59753161bf6af
Kirill <volos88@gmail.com>
2023-05-11 14:39:14 +0300

    add images optimization

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 80,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+    }),
+  ],
   build: {
     outDir: 'docs',
   },
